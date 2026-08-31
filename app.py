7c59484b5806221b0c1f6d2bfec2d68f88339e40
Vértice <labs.vertice@gmail.com>
2026-08-31 13:05:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -364,7 +364,10 @@ if menu == "➕ Novo Nutribook":
             "Selecione o perfil principal do Nutribook:",
             [
                 "Emagrecimento & Definição",
-                "Fertilidade Feminina",
+                "Perfil A",
+                "Perfil B",
+                "Perfil C",
+                "Perfil D",
             ],
             horizontal=False,
         )
